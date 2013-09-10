d7d4477ab616188bfac9b4989979149e212170ea
Mark Grossman <mark.grossman@bridgepointeducation.com>
2013-09-09 18:10:37 -0700

    Add POST route/test + refactor some tests

--- CucumberTests/features/Sample.feature
+++ CucumberTests/features/Sample.feature
@@ -5,8 +5,13 @@ Feature: Sample REST
 
 Scenario: GET to /example
      When I make a get to "/example"
-     Then "firstName" should equal "John"
+     Then the response code should be "200"
+     	And "firstName" should equal "John"
 
 Scenario: GET a route that doesn't exist
 	When I make a get to "/Hello"
 	Then the response code should be "404"
+
+Scenario: POST to a route
+	When I make a post to "/hello" with the name "John"
+	Then "Hello" should equal "John"
--- CucumberTests/features/step_definitions/Sample_Steps.rb
+++ CucumberTests/features/step_definitions/Sample_Steps.rb
@@ -1,18 +1,29 @@
 When /^I make a get to "([^\"]*)"$/ do |path|
 	RestClient.get("#{HOST}#{path}"){ |response, request, result|
-	  case response.code
-	  when 200
-	    @response = JSON.parse(response)
-	  else
-	    @code = response.code
-	  end
+	  handleResponse(response)
 	}
 end
 
 Then /^"([^\"]*)" should equal "([^\"]*)"$/ do |item, expected|
-  expect(@response[item]).to eq(expected)
+	expect(@response[item]).to eq(expected)
 end
 
 Then /^the response code should be "([^\"]*)"$/ do |code|
 	expect(@code.to_s).to eq(code)
 end
+
+When /^I make a post to "([^\"]*)" with the name "([^\"]*)"$/ do |path, data|
+	RestClient.post("#{HOST}#{path}",{"name" => data}.to_json, :content_type => :json, :accept => :json){ |response, request, result|
+		handleResponse(response)
+	}
+end
+
+def handleResponse(response)
+	@code = response.code
+	if @code == 200
+		@response = JSON.parse(response)
+	end
+end
+
+
+
